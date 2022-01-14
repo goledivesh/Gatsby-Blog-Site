173c51817880cc9fa594c9b3d5e3f1b96e4a6cc7
goledivesh <goledivesh@gmail.com>
2022-01-14 22:52:12 +0530

    Testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,9 +131,9 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations Divesh Gole
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! Well Done </span>
+        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
